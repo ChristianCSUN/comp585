e30c72886a941ee6a3d6d5cb76e186bf959c5bc8
NexalyOrellana <70233776+NexalyOrellana@users.noreply.github.com>
2025-04-29 12:43:39 -0700

    update stats page
    - Makes stats page scrollable to better fix different screen sizes

--- lib/pages/stats.dart
+++ lib/pages/stats.dart
@@ -101,222 +101,224 @@ class StatsState extends State<Stats>{
         ),
       ),
       endDrawer: MenuDrawer(),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: OutlinedButton.icon(
-                  onPressed: (){
-                    setState(() {
-                      favToggle = !favToggle;
-                    });
-                    favToggle ? addFavorite(widget.market.name, widget.market.symbol) : removeFavorite(widget.market.symbol);
-                    debugPrint('Favorites Tapped');
-                  },
-                  icon: Icon(
-                    favToggle ? Icons.star_outlined : Icons.star_outline,
-                    color: Colors.yellow, 
-                    size: 35,
-                  ),
-                  label: const AutoSizeText(
-                    "Favorite",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black),
-                  ),
-                  iconAlignment: IconAlignment.start,
-                  style: OutlinedButton.styleFrom(
-                    side: BorderSide(
-                      width: 2,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: OutlinedButton.icon(
+                    onPressed: (){
+                      setState(() {
+                        favToggle = !favToggle;
+                      });
+                      favToggle ? addFavorite(widget.market.name, widget.market.symbol) : removeFavorite(widget.market.symbol);
+                      debugPrint('Favorites Tapped');
+                    },
+                    icon: Icon(
+                      favToggle ? Icons.star_outlined : Icons.star_outline,
+                      color: Colors.yellow, 
+                      size: 35,
                     ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
+                    label: const AutoSizeText(
+                      "Favorite",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black),
                     ),
-                    backgroundColor: favToggle ? Colors.indigo[100] : Colors.transparent,
-                  ),
-                ),
-              ),
-            ),
-            Divider(
-              thickness: 2,
-              color: Colors.black,
-              indent: 10,
-              endIndent: 10,
-            ),
-            SizedBox(
-              height: 50,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column (
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [ 
-                        AutoSizeText(
-                          marketDetails[2]["label"]!,
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxFontSize: 12,
-                          minFontSize: 10,
-                        ),
-                        AutoSizeText(
-                          "\$${marketDetails[2]["value"]}",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxFontSize: 20,
-                          minFontSize: 10,
-                        ),
-                      ],
+                    iconAlignment: IconAlignment.start,
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        width: 2,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      backgroundColor: favToggle ? Colors.indigo[100] : Colors.transparent,
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [ 
-                        AutoSizeText(
-                          "Price Change:",
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxFontSize: 12,
-                          minFontSize: 10,
-                        ),
-                        AutoSizeText(
-                          priceChange > 0 ? "+${(priceChange).toStringAsFixed(2)}" :  (priceChange).toStringAsFixed(2),
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: priceChange > 0 ? Colors.green[700] : Colors.red,
-                          ),
-                          maxFontSize: 20,
-                          minFontSize: 10,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        AutoSizeText(
-                          marketDetails[3]["label"]!,
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxFontSize: 12,
-                          minFontSize: 10,
-                        ),                        
-                        AutoSizeText(
-                          widget.market.percentageChange > 0 ? "(+${marketDetails[3]["value"]}%)" :  "(${marketDetails[3]["value"]}%)",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: widget.market.percentageChange > 0 ? Colors.green[700] : Colors.red,
-                            backgroundColor: widget.market.percentageChange > 0 ? Colors.green[100] : Colors.red[100]
-                          ),
-                          maxFontSize: 20,
-                          minFontSize: 10,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Divider(
-              thickness: 2,
-              color: Colors.black,
-              indent: 10,
-              endIndent: 10,
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3,
-              child: Padding(
-                padding: EdgeInsets.only(left: 10, right: 15),
-                child: Center(
-                  child: StatsGraph(
-                    interval: activeInterval, 
-                    symbol: widget.market.symbol
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Divider(
-              thickness: 2,
-              color: Colors.black,
-              indent: 10,
-              endIndent: 10,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: SizedBox(
+              Divider(
+                thickness: 2,
+                color: Colors.black,
+                indent: 10,
+                endIndent: 10,
+              ),
+              SizedBox(
                 height: 50,
-                child: ListView.builder( 
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 6,
-                  itemBuilder: (context, index){
-                    return OutlinedButton(
-                        onPressed: () => toggleButtonState(index),
-                        style: ElevatedButton.styleFrom(
-                          textStyle: TextStyle(fontSize: 16),
-                          shape: CircleBorder(),
-                          backgroundColor: intervalButtonStates[index] ? Colors.indigo[100] : Colors.transparent,
-                        ), 
-                        child: AutoSizeText("${intervals[index]}"),
-                    );
-                  }
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column (
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [ 
+                          AutoSizeText(
+                            marketDetails[2]["label"]!,
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxFontSize: 12,
+                            minFontSize: 10,
+                          ),
+                          AutoSizeText(
+                            "\$${marketDetails[2]["value"]}",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxFontSize: 20,
+                            minFontSize: 10,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [ 
+                          AutoSizeText(
+                            "Price Change:",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxFontSize: 12,
+                            minFontSize: 10,
+                          ),
+                          AutoSizeText(
+                            priceChange > 0 ? "+${(priceChange).toStringAsFixed(2)}" :  (priceChange).toStringAsFixed(2),
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: priceChange > 0 ? Colors.green[700] : Colors.red,
+                            ),
+                            maxFontSize: 20,
+                            minFontSize: 10,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          AutoSizeText(
+                            marketDetails[3]["label"]!,
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxFontSize: 12,
+                            minFontSize: 10,
+                          ),                        
+                          AutoSizeText(
+                            widget.market.percentageChange > 0 ? "(+${marketDetails[3]["value"]}%)" :  "(${marketDetails[3]["value"]}%)",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: widget.market.percentageChange > 0 ? Colors.green[700] : Colors.red,
+                              backgroundColor: widget.market.percentageChange > 0 ? Colors.green[100] : Colors.red[100]
+                            ),
+                            maxFontSize: 20,
+                            minFontSize: 10,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Divider(
-              thickness: 2,
-              color: Colors.black,
-              indent: 10,
-              endIndent: 10,
-            ),
-            SizedBox(
-              height: 200,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child:ListView.builder( 
-                  itemCount: 6,
-                  itemBuilder: (context, index){
-                    final item = marketDetails[index + 4];
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        AutoSizeText(
-                          "${item["label"]!}:",
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            ),
-                          maxFontSize: 30,
-                          minFontSize: 10,
-                        ),
-                        AutoSizeText(
-                          item["value"]!,
-                          style: TextStyle(
-                            fontSize: 22,
-                            ),
-                          maxFontSize: 30,
-                          minFontSize: 10,
-                        ),
-                      ],
-                    );
-                  }
+              Divider(
+                thickness: 2,
+                color: Colors.black,
+                indent: 10,
+                endIndent: 10,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10, right: 15),
+                  child: Center(
+                    child: StatsGraph(
+                      interval: activeInterval, 
+                      symbol: widget.market.symbol
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 10),
+              Divider(
+                thickness: 2,
+                color: Colors.black,
+                indent: 10,
+                endIndent: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: SizedBox(
+                  height: 50,
+                  child: ListView.builder( 
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 6,
+                    itemBuilder: (context, index){
+                      return OutlinedButton(
+                          onPressed: () => toggleButtonState(index),
+                          style: ElevatedButton.styleFrom(
+                            textStyle: TextStyle(fontSize: 16),
+                            shape: CircleBorder(),
+                            backgroundColor: intervalButtonStates[index] ? Colors.indigo[100] : Colors.transparent,
+                          ), 
+                          child: AutoSizeText("${intervals[index]}"),
+                      );
+                    }
+                  ),
+                ),
+              ),
+              Divider(
+                thickness: 2,
+                color: Colors.black,
+                indent: 10,
+                endIndent: 10,
+              ),
+              SizedBox(
+                height: 200,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child:ListView.builder( 
+                    itemCount: 6,
+                    itemBuilder: (context, index){
+                      final item = marketDetails[index + 4];
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          AutoSizeText(
+                            "${item["label"]!}:",
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              ),
+                            maxFontSize: 30,
+                            minFontSize: 10,
+                          ),
+                          AutoSizeText(
+                            item["value"]!,
+                            style: TextStyle(
+                              fontSize: 22,
+                              ),
+                            maxFontSize: 30,
+                            minFontSize: 10,
+                          ),
+                        ],
+                      );
+                    }
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
